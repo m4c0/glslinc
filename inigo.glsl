2ec6630cb861f72f8b55dd1528e0fe361b51ab37
Eduardo Costa <m4c0@users.noreply.github.com>
2024-11-01 21:03:30 +0000

    adding some inigo routines

---
+++ inigo.glsl
@@ -0,0 +1,22 @@
+// Based on routines created by Inigo Quilez
+
+float sd_box(vec2 p, vec2 b) {
+  vec2 d = abs(p) - b;
+  return length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
+}
+
+float sd_rnd_box(vec2 p, vec2 b, float r) {
+  return sd_box(p, b) - r;
+}
+
+vec3 inigo_debug(float d) {
+  // this is 2.0 / resolution.y in Shader Toy. Impacts line thickness.
+  const float px = 2.0 / 600.0;
+
+  vec3 col = (d>0.0) ? vec3(0.9,0.6,0.3) : vec3(0.65,0.85,1.0);
+  col *= 1.0 - exp2(-24.0*abs(d));
+  col *= 0.8 + 0.2*cos(120.0*d);
+  col = mix( col, vec3(1.0), 1.0-smoothstep(-px,px,abs(d)-0.005) );
+  return pow(col, vec3(2.2));
+}
+
