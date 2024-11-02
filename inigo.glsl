8ebe7ca3a4c298f00b899b82e11adae4b5792ee4
Eduardo Costa <m4c0@users.noreply.github.com>
2024-11-02 12:46:08 +0000

    circle

--- inigo.glsl
+++ inigo.glsl
@@ -1,5 +1,9 @@
 // Based on routines created by Inigo Quilez
 
+float sd_circle(vec2 p, float r) {
+  return length(p) - r;
+}
+
 float sd_box(vec2 p, vec2 b) {
   vec2 d = abs(p) - b;
   return length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
