f617de0faca00818e160c317aa9067c5b4c0de76
Eduardo Costa <m4c0@users.noreply.github.com>
2024-11-09 13:10:59 +0000

    uneven caps

--- inigo.glsl
+++ inigo.glsl
@@ -64,6 +64,16 @@ float sd_segment(vec2 p, vec2 a, vec2 b) {
   return length(pa - ba * h);
 }
 
+float sd_uneven_capsule(vec2 p, float r1, float r2, float h) {
+  p.x = abs(p.x);
+  float b = (r1 - r2) / h;
+  float a = sqrt(1.0 - b * b);
+  float k = dot(p, vec2(-b, a));
+  if (k < 0.0) return length(p) - r1;
+  if (k > a * h) return length(p - vec2(0.0, h)) - r2;
+  return dot(p, vec2(a, b)) - r1;
+}
+
 float sd_tunnel(vec2 p, vec2 wh) {
   p.x = abs(p.x);
   p.y = -p.y;
