8db635377e94550b235d2b70014fdc2113681600
Eduardo Costa <m4c0@users.noreply.github.com>
2024-11-21 12:39:42 +0000

    box_3d

--- inigo.glsl
+++ inigo.glsl
@@ -8,6 +8,11 @@ float sd_circle(vec2 p, float r) {
 float sd_box(vec2 p, vec2 b) {
   vec2 d = abs(p) - b;
   return length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
+} 
+
+float sd_box_3d(vec3 p, vec3 b, float r) {
+  vec3 q = abs(p) - b + r;
+  return length(max(q, 0)) + min(max(q.x, max(q.y, q.z)), 0.0) - r;
 }
 
 float sd_cut_disk(vec2 p, float r, float h) {
