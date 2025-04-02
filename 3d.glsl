f58883a15943256ab1140775e2fba31b2c696944
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-03 00:20:07 +0100

    making the translation matrix more intuitive

--- 3d.glsl
+++ 3d.glsl
@@ -1,3 +1,12 @@
+mat4 translation(vec3 p) {
+  return mat4(
+    vec4(1, 0, 0, p.x),
+    vec4(0, 1, 0, p.y),
+    vec4(0, 0, 1, p.z),
+    vec4(0, 0, 0, 1)
+  );
+}
+
 mat4 projection_matrix(float fov_rad, float aspect, float near, float far) {
   const float f = 1 / atan(fov_rad / 2);
   const float p22 = (far + near) / (near - far);
@@ -14,12 +23,7 @@ mat4 view_matrix(vec3 cam, float a, vec3 up) {
   vec3 f = normalize(vec3(sin(a), 0, cos(a)));
   vec3 s = normalize(cross(f, up));
   vec3 u = cross(s, f);
-  mat4 trn = mat4(
-    vec4(1, 0, 0, -cam.x),
-    vec4(0, 1, 0, -cam.y),
-    vec4(0, 0, 1, -cam.z),
-    vec4(0, 0, 0, 1)
-  );
+  mat4 trn = translation(-cam);
   mat4 rot = mat4(
     vec4( s, 0),
     vec4( u, 0),
